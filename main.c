0323ff8a08f291dfd45b676a131db84586921315
faalvarez <faalvarez@192.168.1.158>
2017-04-07 11:12:29 +0200

    Empezando

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Tarzkan
+ *
+ * Created on 7 de abril de 2017, 11:11
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
